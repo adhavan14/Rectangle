1788568ad425b03d2ec60b085bf1e755cb0aa4de
adhavan14 g <adhavanganesan14@gmail.com>
2023-10-24 12:59:41 +0000

    Update the name of the test method

--- src/test/java/com/thoughtworks/rectangle/RectangleTest.java
+++ src/test/java/com/thoughtworks/rectangle/RectangleTest.java
@@ -7,14 +7,14 @@ import org.junit.jupiter.api.Test;
 public class RectangleTest {
 
     @Test
-    public void shouldAreaOfTheRectangleIsZeroWhenAnyValueIsZero() {
+    public void shouldReturnAreaOf0WhenLengthAndBreadthIs0() {
         Rectangle rectangle = new Rectangle(0,5);
         int result = rectangle.area();
         assertEquals(0, result);
     }
 
     @Test
-    public void shouldAreaOfTheRectangleIsTenWhenLengthIsTwoAndBreadthIsFive() {
+    public void shouldReturnAreaOf10WhenLengthIs2AndBreadthIs5() {
         Rectangle rectangle = new Rectangle(2,5);
         int result = rectangle.area();
         assertEquals(10, result);
