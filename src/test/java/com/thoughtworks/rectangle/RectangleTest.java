bc8dfa7b6a9eadd75f9b702d8d89a56580f4e7c3
adhavan14 g <adhavanganesan14@gmail.com>
2023-10-24 13:57:50 +0000

    Refactor. the assert statement in the test method

--- src/test/java/com/thoughtworks/rectangle/RectangleTest.java
+++ src/test/java/com/thoughtworks/rectangle/RectangleTest.java
@@ -10,18 +10,16 @@ public class RectangleTest {
 
     @Test
     public void shouldReturnAreaOf0WhenLengthOrBreadthIs0() {
-        Integer ACTUAL = 0;
         Rectangle rectangle = new Rectangle(0,5);
-        Integer EXPECTED = rectangle.area();
-        assertThat(ACTUAL, is(equalTo(EXPECTED)));
+        int area = rectangle.area();
+        assertThat(area, is(equalTo(0)));
     }
 
     @Test
     public void shouldReturnAreaOf10WhenLengthIs2AndBreadthIs5() {
 
-        Integer ACTUAL = 10;
         Rectangle rectangle = new Rectangle(2,5);
-        Integer EXPECTED = rectangle.area();
-        assertThat(ACTUAL, is(equalTo(EXPECTED)));
+        int area = rectangle.area();
+        assertThat(area, is(equalTo(10)));
     }
 }
