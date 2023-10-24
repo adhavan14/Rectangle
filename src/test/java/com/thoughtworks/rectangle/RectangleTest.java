f31e1e59a5e7ee0363f16590d981e7615da7e798
adhavan14 g <adhavanganesan14@gmail.com>
2023-10-24 13:53:12 +0000

    Refactor. the assert statement in the test method

--- src/test/java/com/thoughtworks/rectangle/RectangleTest.java
+++ src/test/java/com/thoughtworks/rectangle/RectangleTest.java
@@ -2,17 +2,18 @@ package com.thoughtworks.rectangle;
 
 import static org.hamcrest.MatcherAssert.assertThat;
 import static org.hamcrest.Matchers.equalTo;
+import static org.hamcrest.Matchers.is;
 
 import org.junit.jupiter.api.Test;
 
 public class RectangleTest {
 
     @Test
-    public void shouldReturnAreaOf0WhenLengthAndBreadthIs0() {
+    public void shouldReturnAreaOf0WhenLengthOrBreadthIs0() {
         Integer ACTUAL = 0;
         Rectangle rectangle = new Rectangle(0,5);
         Integer EXPECTED = rectangle.area();
-        assertThat(ACTUAL, equalTo(EXPECTED));
+        assertThat(ACTUAL, is(equalTo(EXPECTED)));
     }
 
     @Test
@@ -21,6 +22,6 @@ public class RectangleTest {
         Integer ACTUAL = 10;
         Rectangle rectangle = new Rectangle(2,5);
         Integer EXPECTED = rectangle.area();
-        assertThat(ACTUAL, equalTo(EXPECTED));
+        assertThat(ACTUAL, is(equalTo(EXPECTED)));
     }
 }
